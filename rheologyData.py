16ab9239bc14129cdce979b72501062f611c4fd8
Mike <mrv@uoregon.edu>
2018-07-31 13:45:35 -0500

    I changed rheologyData functions to return more data than just viscosity as a function of shear stress.

--- rheologyData.py
+++ rheologyData.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 23 10:28:17 2018
+
+@author: Mike van der Naald
+"""
+
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jun 11 16:28:27 2018
 
-This package is a bunch of functions that generates a shear-jamming phase diagram.
+This package is a bunch of functions that first parses the data from the Anton-Parr
+rheometer into a series of numpy arrays and saves them (rheologyDataParserOld).
+
+Once the data is saved into a series of numpy arrays all of the arrays can be averaged together
+and saved into an averaged numpy array using dataSetPackager.
+
+All of the data 
 """
 
 import numpy as np 
@@ -12,19 +26,24 @@ from scipy.optimize import curve_fit
 
 
 
-def oldRheologyDataParser(fileName,outputDirectory):    
+def rheologyDataParserOld( fileName, outputDirectory ):    
     """
     This function takes the data from the old format of the Anton-Parr rheometer and parses it into a series of text files that has
     less information but also makes the data easier to deal with.  To get a feeling
     for what this function is doing take a look at the old format and see how it is arranged first.
     
-    This package is a bunch of functions that generates a shear-jamming phase diagram.
+    The fileName is the fileName of the textfile that was outputted from the Anton-Parr program that is in the format 
+    that is obtained when one does file-->export-->contents of active window-->tab separated text file.  The file exported
+    should all be of the same packing fraction.  The output directory is where each data set is outputted as a numpy 
+    saved file.  Each numpy file has a [N,6] array in it where N is the number of data points taken and the 6 correpsonds to 
+    the 6 quantities measured during a constant shear rate experiment [point # , time, viscosity, shear rate, shear stress, torque].
+    
     """
     
     
     #Loading in the file to parse.
     #We read the whole file as a list of lines to make the parsing easier.    
-    with open(fileName, "r") as f1:
+    with open(fileName, "rb") as f1:
          lines = f1.readlines()
          
     
@@ -33,7 +52,7 @@ def oldRheologyDataParser(fileName,outputDirectory):
     linesWithDataSeriesInfo = np.zeros([0])
     counter = 0
     for line in lines:
-        if "Data Series Information\n" in line:
+        if "Data Series Information\r\n" in line.decode(encoding="utf-8", errors='ignore'):
             linesWithDataSeriesInfo= np.append(linesWithDataSeriesInfo , counter)
         counter = counter + 1
         
@@ -42,7 +61,6 @@ def oldRheologyDataParser(fileName,outputDirectory):
     totalNumOfExperiments = len(linesWithDataSeriesInfo)
 
     for i in range(0,totalNumOfExperiments):
-        print("i equals " + str(i))
         #This picks out one of the blocks of data and now we need to parse it into 
         #the separate experiments and the name of the experiement.  These will go into 
         #a text file that is in a directory that is named after the experiment 
@@ -56,9 +74,10 @@ def oldRheologyDataParser(fileName,outputDirectory):
         #First we"ll get the name of the experiment by looping through all of the rows of the data set
         #and finding the name.
         for j in range(0, len(currentDataSet)):
-            if "Name:" in currentDataSet[j]:
-                nameOfCurrentDataSet = currentDataSet[j][8:].strip("\n").replace(" ", "")
+            if "Name:" in currentDataSet[j].decode(encoding="utf-8", errors='ignore'):
+                nameOfCurrentDataSet = currentDataSet[j][8:]
         
+        nameOfCurrentDataSet = nameOfCurrentDataSet.decode(encoding="utf-8", errors='ignore').replace("\r\n","")
         #Now that we have the name of the dataset we can make a new directory 
         #inside the output directory with the name of the experiment.
         filePath = (outputDirectory+os.sep+nameOfCurrentDataSet)
@@ -73,7 +92,7 @@ def oldRheologyDataParser(fileName,outputDirectory):
         #because that is the beginning of a dataset.
         linesWhereDataStarts = np.zeros([0])
         for k in range(0, len(currentDataSet)):
-            if "\t[s]\t[Pa·s]\t[1/s]\t[Pa]\t[mNm]\t[]\n" in currentDataSet[k]:
+            if "\t[s]\t[Pas]\t[1/s]\t[Pa]\t[mNm]\t[]\r\n" in currentDataSet[k].decode(encoding="utf-8", errors='ignore'):
                 linesWhereDataStarts = np.append(linesWhereDataStarts, k)
                 
                 
@@ -83,14 +102,15 @@ def oldRheologyDataParser(fileName,outputDirectory):
         linesWhereDataEnds = np.zeros([0])
         for j in linesWhereDataStarts:
             for k in range(j,len(currentDataSet)):
-                if currentDataSet[k]=="\n":
+                if currentDataSet[k].decode(encoding="utf-8", errors='ignore') == "\r\n":
                     linesWhereDataEnds = np.append(linesWhereDataEnds, k)
                     break
-                
+        
+        linesWhereDataEnds=[int(i) for i in linesWhereDataEnds]  
+        
         if len(linesWhereDataEnds)!=len(linesWhereDataStarts):
             linesWhereDataEnds = np.append(linesWhereDataEnds, len(currentDataSet) )
-                                
-        linesWhereDataEnds=[int(i) for i in linesWhereDataEnds]  
+            
         
         for j in range(0,len(linesWhereDataEnds)):
 
@@ -99,29 +119,13 @@ def oldRheologyDataParser(fileName,outputDirectory):
             finalDataSet = np.zeros((len(dataSetToSave),6))
 
             for k in range(0,len(dataSetToSave)):
-                finalDataSet[k,:]=np.fromstring(dataSetToSave[k].replace('\t',' ').replace(',', '').replace('\n',''),sep=' ')
+                finalDataSet[k,:]=np.fromstring(dataSetToSave[k].decode(encoding="utf-8", errors='ignore').replace('\t',' ').replace(',', '').replace('\n',''),sep=' ')
             finalNameForData = r"dataSet-" + str(j) + r".txt"
             filePathFinal = (outputDirectory+os.sep+nameOfCurrentDataSet+os.sep+finalNameForData)
             np.save(filePathFinal,finalDataSet)
             
-            
-    
-def dataSetAverager(x,y):
-    """This function takes in multiple data sets and averages them and returns their error as well.  
-    x should be of the shape (N,numPoints) where N is the number of experiments done and numPoints is the data points collected
-    
-    """
 
-    xAverage = np.mean(x,axis=0)    
-    yAverage = np.mean(y,axis=0) 
-    
-    xError = np.std(x,axis=0)
-    yError = np.std(y,axis=0)
-    
-    return (xAverage,yAverage,xError,yError)
-
-
-def stressVsViscosityDataSetPackager(topDir):
+def dataSetPackager(topDir):
     """
     This function takes the path topDir and finds all of the data files that end in .npy, these files should have been generated by
     the above function "oldRheologyDataParser".  That function takes the raw rheometer data and packages it into an array that has rows 
@@ -136,53 +140,40 @@ def stressVsViscosityDataSetPackager(topDir):
         directories MAKE SURE TO PUT r BEFORE THE STRING, FOR EXAMPLE topDir = r"blah"
         
     outputs:
-        averagedShearSress the averaged shear stress from all the flow curves found in topDir 
-        
-        averagedViscosity the averaged viscosity from all the flow curves found in topDir 
-        
-        errorsInShearSress is the standard deviation of the shear stresses averaged from the flow curves found in topDir 
-        
-        errorsInViscosity is the standard deviation of the averaged viscosities from the flow curves found in topDir
+        It
+
         
     """
     
     #Intialize the containers that will hold all of the data sets.
-    shearStressHolder = []
-    viscosityHolder = []
+    dataSetHolder = np.array([])
     
     #This loop will find all files in topDir that end with .npy
     for dirpath, dirnames, filenames in os.walk(topDir):
         for filename in [f for f in filenames if f.endswith(r".npy")]:
             
-            if filename == r"averagedShearStressVsViscosity.npy":
+            if (filename == r"averagedData.npy") or (filename == r"errorsData.npy"):
                 break
             #This is the current dataset that is organized as [pt#, time, viscosity, shear rate, shear stress, torque]
-            currentDataSet = np.load(os.path.join(dirpath, filename))
+            currentDataSet = np.expand_dims(np.load(os.path.join(dirpath, filename)),axis=2)
             
             #The current data set should be ordered from lowest controlled shear rate to highest shear rate so we check and then flip if need be.
-            if currentDataSet[0,4] > currentDataSet[-1,4]:
+            if currentDataSet[0,4,0] > currentDataSet[-1,4,0]:
                 currentDataSet = np.flip(currentDataSet,axis=0)
                         
-            if viscosityHolder == []:
-                viscosityHolder = np.expand_dims(currentDataSet[:,2],axis=1)
+            if dataSetHolder.any():
+                dataSetHolder = np.append(dataSetHolder , currentDataSet , axis=2 )
             else:
-                viscosityHolder = np.append(viscosityHolder , np.expand_dims(currentDataSet[:,2],axis=1) , axis=1 )
-                 
+                dataSetHolder = currentDataSet
                 
-            if shearStressHolder == []:
-                shearStressHolder = np.expand_dims(currentDataSet[:,4],axis=1)
-            else:
-                shearStressHolder = np.append(shearStressHolder , np.expand_dims(currentDataSet[:,4],axis=1) , axis=1 )
-                
+    
+    averagedData = np.mean(dataSetHolder,axis=2)
+    standardDeviationData = np.std(dataSetHolder,axis=2)
+    
+    np.save(os.path.join(topDir, r"averagedData"), averagedData )
+    np.save(os.path.join(topDir, r"errorsData"), standardDeviationData )
 
-    
-    
-    
-    (averagedShearSress, averagedViscosity, errorsInShearSress,errorsInViscosity) = dataSetAverager( np.transpose(shearStressHolder) , np.transpose(viscosityHolder) )
-    
-    np.save(os.path.join(topDir, r"averagedShearStressVsViscosity"), np.concatenate( (np.expand_dims(averagedShearSress,axis=1) ,np.expand_dims(averagedViscosity,axis=1),np.expand_dims(errorsInShearSress,axis=1),np.expand_dims(errorsInViscosity,axis=1)), axis=1 ) )
-
-    return (averagedShearSress, averagedViscosity, errorsInShearSress,errorsInViscosity)
+    return (averagedData,standardDeviationData)
             
             
     
@@ -198,21 +189,28 @@ def shearStressVsViscosityPlotter(topDir, maxMinPtsOn=False):
     
     for packingFraction in listOfSubDirs:
         
-        loadInAveragedData = np.load(os.path.join(topDir, packingFraction , r"averagedShearStressVsViscosity.npy" ))
         
-        pyplot.errorbar(loadInAveragedData[:,0],loadInAveragedData[:,1],loadInAveragedData[:,2],loadInAveragedData[:,3],label=packingFraction,marker='o')
+        loadInAveragedData = np.load(os.path.join(topDir, packingFraction , r"averagedData.npy" ))
+        loadInErrordata = np.load(os.path.join(topDir, packingFraction , r"errorsData.npy" ))
         
+        #We want to plot the viscosity both as a function of shear stress and shear rate.
+        
+        #First the plot as a function of shear stress.
+        pyplot.errorbar(loadInAveragedData[:,4],loadInAveragedData[:,2],xerr= loadInErrordata[:,4],yerr=loadInErrordata[:,2],label=packingFraction,marker='o')
+        #Next plot the viscosity as a function of shear rate,
+
         if maxMinPtsOn==True:
 
-            pyplot.plot(max(loadInAveragedData[:,0]),max(loadInAveragedData[:,1]),marker='o',markersize=12,color='red' )
-            pyplot.plot(min(loadInAveragedData[:,0]),min(loadInAveragedData[:,1]),marker='o',markersize=12,color='red' )
+            pyplot.plot(max(loadInAveragedData[:,4]),max(loadInAveragedData[:,2]),marker='o',markersize=12,color='red' )
+            pyplot.plot(min(loadInAveragedData[:,4]),min(loadInAveragedData[:,2]),marker='o',markersize=12,color='red' )
             
             
             if phiVsMinViscosity.size == 0:
-                phiVsMinViscosity = np.array([float(packingFraction) ,min(loadInAveragedData[:,1])])
-                phiVsMaxViscosity = np.array([float(packingFraction) ,max(loadInAveragedData[:,1])])   
+                
+                phiVsMinViscosity = np.array([float(packingFraction) ,min(loadInAveragedData[:,2])])
+                phiVsMaxViscosity = np.array([float(packingFraction) ,max(loadInAveragedData[:,2])])   
             else:
-                print(phiVsMinViscosity)
+                
                 phiVsMinViscosity = np.vstack( (phiVsMinViscosity,np.array([float(packingFraction) ,min(loadInAveragedData[:,1])])))
                 phiVsMaxViscosity = np.vstack( (phiVsMaxViscosity,np.array([float(packingFraction) ,max(loadInAveragedData[:,1])])) )
                 
@@ -220,23 +218,24 @@ def shearStressVsViscosityPlotter(topDir, maxMinPtsOn=False):
     
     pyplot.yscale('log')
     pyplot.xscale('log')
-    pyplot.xlabel("Shear Stress [Pa]")
+    pyplot.xlabel("Shear Rate [1/s]")
     pyplot.ylabel("Viscosity [Pa X s]")
     pyplot.grid(True)
     pyplot.legend()
+    
+    
     if maxMinPtsOn == True:
+        #Sort the viscosities based no the entry in the first column.
+        phiVsMinViscosity = phiVsMinViscosity[phiVsMinViscosity[:,0].argsort()]
+        phiVsMaxViscosity = phiVsMaxViscosity[phiVsMaxViscosity[:,0].argsort()]
         return (phiVsMinViscosity,phiVsMaxViscosity)
         
-    
-    
-        
-
 
 def fitAlphaPhiJ(viscosityValues, packingFractionValues, suspendingViscosity):
     
     def divergingViscosityFunc(x,phiJ, alpha):
         
-        return suspendingViscosity * ( 1 - x / phiJ )**(-alpha)
+        return suspendingViscosity * ( 1 - x / phiJ )**( -alpha )
     
     
     (popt, pcov) = curve_fit(divergingViscosityFunc, packingFractionValues, viscosityValues)
