36164cdf62a68d7d48bd1e9d8e57ab701ca9bac9
Mike <mrv@uoregon.edu>
2018-08-15 09:24:01 -0500

    I broke the package rheologyData into two different packages.  One called antonParrDataParser contains the two functions needed to parse data from the Anton-Parr rheometer into numpy files.  The second is dataPlotsFits which contains various functions to plot and fit the resulting data in various ways.  I still need to include a detailed comments and a readme on how to use the functions in both of these packages.

---
+++ antonParrDataParser.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 09:17:23 2018
+
+@author: mike
+"""
+
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 23 10:28:17 2018
+
+@author: Mike van der Naald
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 11 16:28:27 2018
+
+This package is a bunch of functions that first parses the data from the Anton-Parr
+rheometer into a series of numpy arrays and saves them (rheologyDataParserOld).
+
+Once the data is saved into a series of numpy arrays all of the arrays can be averaged together
+and saved into an averaged numpy array using dataSetPackager.
+
+All of the data 
+"""
+
+import numpy as np 
+import os
+from matplotlib import pyplot
+from scipy.optimize import curve_fit
+
+
+def rheologyDataParserOld( fileName, outputDirectory ):    
+    """
+    This function takes the data from the old format of the Anton-Parr rheometer and parses it into a series of text files that has
+    less information but also makes the data easier to deal with.  To get a feeling
+    for what this function is doing take a look at the old format and see how it is arranged first.
+    
+    The fileName is the fileName of the textfile that was outputted from the Anton-Parr program that is in the format 
+    that is obtained when one does file-->export-->contents of active window-->tab separated text file.  The file exported
+    should all be of the same packing fraction.  The output directory is where each data set is outputted as a numpy 
+    saved file.  Each numpy file has a [N,6] array in it where N is the number of data points taken and the 6 correpsonds to 
+    the 6 quantities measured during a constant shear rate experiment [point # , time, viscosity, shear rate, shear stress, torque].
+    
+    """
+    
+    
+    #Loading in the file to parse.
+    #We read the whole file as a list of lines to make the parsing easier.    
+    with open(fileName, "rb") as f1:
+         lines = f1.readlines()
+         
+    
+    #We need to go through and find all of the lines that have "Data Series Information\n"
+    #in them because these lines are the ones that segment our data into different experiments.
+    linesWithDataSeriesInfo = np.zeros([0])
+    counter = 0
+    for line in lines:
+        if "Data Series Information\r\n" in line.decode(encoding="utf-8", errors='ignore'):
+            linesWithDataSeriesInfo= np.append(linesWithDataSeriesInfo , counter)
+        counter = counter + 1
+        
+        
+    #Now we know the total number of datasets we have is:
+    totalNumOfExperiments = len(linesWithDataSeriesInfo)
+
+    for i in range(0,totalNumOfExperiments):
+        #This picks out one of the blocks of data and now we need to parse it into 
+        #the separate experiments and the name of the experiement.  These will go into 
+        #a text file that is in a directory that is named after the experiment 
+
+        if i == totalNumOfExperiments-1:
+            currentDataSet = lines[ int(linesWithDataSeriesInfo[i]) : int(len(lines)) ]
+        else:
+            currentDataSet = lines[ int(linesWithDataSeriesInfo[i]) : int(linesWithDataSeriesInfo[i+1]) ]
+        
+        #Now we need to pick through this data set and get all of the information we want out of it.
+        #First we"ll get the name of the experiment by looping through all of the rows of the data set
+        #and finding the name.
+        for j in range(0, len(currentDataSet)):
+            if "Name:" in currentDataSet[j].decode(encoding="utf-8", errors='ignore'):
+                nameOfCurrentDataSet = currentDataSet[j][8:]
+        
+        nameOfCurrentDataSet = nameOfCurrentDataSet.decode(encoding="utf-8", errors='ignore').replace("\r\n","")
+        #Now that we have the name of the dataset we can make a new directory 
+        #inside the output directory with the name of the experiment.
+        filePath = (outputDirectory+os.sep+nameOfCurrentDataSet)
+        os.mkdir(filePath)
+        
+        
+        
+        
+        
+        #Now we want to loop through and find all the lines in our data set that have
+        # "\t[s]\t[Pa·s]\t[1/s]\t[Pa]\t[mNm]\t[]\n"
+        #because that is the beginning of a dataset.
+        linesWhereDataStarts = np.zeros([0])
+        for k in range(0, len(currentDataSet)):
+            if "\t[s]\t[Pas]\t[1/s]\t[Pa]\t[mNm]\t[]\r\n" in currentDataSet[k].decode(encoding="utf-8", errors='ignore'):
+                linesWhereDataStarts = np.append(linesWhereDataStarts, k)
+                
+                
+        linesWhereDataStarts=[int(i) for i in linesWhereDataStarts]     
+        #Now that we know where the data starts we can start to pick out individual datasets
+
+        linesWhereDataEnds = np.zeros([0])
+        for j in linesWhereDataStarts:
+            for k in range(j,len(currentDataSet)):
+                if currentDataSet[k].decode(encoding="utf-8", errors='ignore') == "\r\n":
+                    linesWhereDataEnds = np.append(linesWhereDataEnds, k)
+                    break
+        
+        linesWhereDataEnds=[int(i) for i in linesWhereDataEnds]  
+        
+        if len(linesWhereDataEnds)!=len(linesWhereDataStarts):
+            linesWhereDataEnds = np.append(linesWhereDataEnds, len(currentDataSet) )
+            
+        
+        for j in range(0,len(linesWhereDataEnds)):
+
+            filePathFinal = (outputDirectory+os.sep+nameOfCurrentDataSet)
+            dataSetToSave = currentDataSet[linesWhereDataStarts[j]+1:linesWhereDataEnds[j]]
+            finalDataSet = np.zeros((len(dataSetToSave),6))
+
+            for k in range(0,len(dataSetToSave)):
+                finalDataSet[k,:]=np.fromstring(dataSetToSave[k].decode(encoding="utf-8", errors='ignore').replace('\t',' ').replace(',', '').replace('\n',''),sep=' ')
+            finalNameForData = r"dataSet-" + str(j) + r".txt"
+            filePathFinal = (outputDirectory+os.sep+nameOfCurrentDataSet+os.sep+finalNameForData)
+            np.save(filePathFinal,finalDataSet)
+            
+
+def dataSetPackager(topDir):
+    """
+    This function takes the path topDir and finds all of the data files that end in .npy, these files should have been generated by
+    the above function "oldRheologyDataParser".  That function takes the raw rheometer data and packages it into an array that has rows 
+    that are arranged as [pt#, time, viscosity, shear rate, shear stress, torque].  This function loads in each individual flow curve generated
+    by "oldRheologyDataParser" and averages together the xvalues (shear stress) and averages together the yvalues (viscosity) and generates
+    errors for averaged data point.  
+    
+    YOU MUST DELETE YOUR PRESHEAR .npy FILES FROM YOUR topDir DIRECTORY OR ELSE THIS FUNCTION WILL TRY TO AVERAGE THOSE IN
+    
+    inputs:
+        topDir is the directory in which all of the flow curves that you would like to average together.  Delete the preshears from this
+        directories MAKE SURE TO PUT r BEFORE THE STRING, FOR EXAMPLE topDir = r"blah"
+        
+    outputs:
+        It
+
+        
+    """
+    
+    #Intialize the containers that will hold all of the data sets.
+    dataSetHolder = np.array([])
+    
+    #This loop will find all files in topDir that end with .npy
+    for dirpath, dirnames, filenames in os.walk(topDir):
+        for filename in [f for f in filenames if f.endswith(r".npy")]:
+            
+            if (filename == r"averagedData.npy") or (filename == r"errorsData.npy"):
+                break
+            #This is the current dataset that is organized as [pt#, time, viscosity, shear rate, shear stress, torque]
+            currentDataSet = np.expand_dims(np.load(os.path.join(dirpath, filename)),axis=2)
+            
+            #The current data set should be ordered from lowest controlled shear rate to highest shear rate so we check and then flip if need be.
+            if currentDataSet[0,4,0] > currentDataSet[-1,4,0]:
+                currentDataSet = np.flip(currentDataSet,axis=0)
+            print(dirpath+os.sep+filename)
+            if dataSetHolder.any():
+                dataSetHolder = np.append(dataSetHolder , currentDataSet , axis=2 )
+            else:
+                dataSetHolder = currentDataSet
+                
+    
+    averagedData = np.mean(dataSetHolder,axis=2)
+    standardDeviationData = np.std(dataSetHolder,axis=2)
+    
+    np.save(os.path.join(topDir, r"averagedData"), averagedData )
+    np.save(os.path.join(topDir, r"errorsData"), standardDeviationData )
+
+    return (averagedData,standardDeviationData)
+            
+            
---
+++ dataPlotsFits.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 09:17:41 2018
+
+@author: mike
+"""
+
+import numpy as np 
+import os
+from matplotlib import pyplot
+from scipy.optimize import curve_fit
+
+def shearStressVsViscosityPlotter(topDir, maxMinPtsOn=False,minViscosityList=False,maxViscosityList=False,suspendingViscosity=False):
+    
+    
+    #Get a list of all the subdirectories
+    listOfSubDirs = next(os.walk(topDir))[1]
+    listOfSubDirs = [float(i) for i in listOfSubDirs]
+    listOfSubDirs.sort()
+    listOfSubDirs = [str(i) for i in listOfSubDirs]
+    listOfSubDirs = listOfSubDirs[::-1]
+    
+    if maxMinPtsOn == True:
+        if (len(minViscosityList)!=len(listOfSubDirs)) or (len(maxViscosityList)!=len(listOfSubDirs)):
+            return print("The length of minViscosityList and the length of maxViscosityList must be the same as the number of packing fractions in topDir.")
+        
+
+
+    if maxMinPtsOn==True:
+        phiVsMaxViscosity = np.array([])
+        phiVsMinViscosity = np.array([])
+        
+        
+    ax = pyplot.subplot(111)
+    
+    counter = 0
+    for packingFraction in listOfSubDirs:
+        
+        
+        loadInAveragedData = np.load(os.path.join(topDir, packingFraction , r"averagedData.npy" ))
+        loadInErrordata = np.load(os.path.join(topDir, packingFraction , r"errorsData.npy" ))
+        
+        #We want to plot the viscosity both as a function of shear stress and shear rate.
+        
+        #First the plot as a function of shear stress.
+        ax.errorbar(loadInAveragedData[:,4],loadInAveragedData[:,2],xerr= loadInErrordata[:,4],yerr=loadInErrordata[:,2],label=packingFraction,marker='o')
+        #Next plot the viscosity as a function of shear rate,
+
+        if maxMinPtsOn==True:
+            sortedViscosities = np.sort(loadInAveragedData[:,2])
+            
+            if minViscosityList[counter] == -1:
+                minViscosity = np.nan
+            else:
+                minViscosity = sortedViscosities[minViscosityList[counter]]
+                
+            if maxViscosityList[counter] == -1:
+                maxViscosity = np.nan
+            else:
+                maxViscosity = sortedViscosities[-(maxViscosityList[counter]+1)]
+                    
+
+            if minViscosityList[counter] != -1:
+                shearStressForMinViscosity = loadInAveragedData[np.where(loadInAveragedData[:,2]==minViscosity)[0][0],4]
+                ax.plot(shearStressForMinViscosity,minViscosity,marker='o',markersize=10,color='blue',mfc='none' , zorder=10 )
+            
+            if maxViscosityList[counter] != -1:
+                shearStressForMaxViscosity = loadInAveragedData[np.where(loadInAveragedData[:,2]==maxViscosity)[0][0],4]
+                ax.plot(shearStressForMaxViscosity,maxViscosity,marker='o',markersize=10,color='red',mfc='none', zorder=10  )
+            counter = counter +1
+            
+            if phiVsMinViscosity.size == 0:
+                
+                phiVsMinViscosity = np.array([float(packingFraction)/100 ,minViscosity])
+                phiVsMaxViscosity = np.array([float(packingFraction)/100 ,maxViscosity])   
+            else:
+                
+                phiVsMinViscosity = np.vstack( (phiVsMinViscosity,np.array([float(packingFraction)/100 ,minViscosity])))
+                phiVsMaxViscosity = np.vstack( (phiVsMaxViscosity,np.array([float(packingFraction)/100 ,maxViscosity])) )
+
+
+    if maxMinPtsOn == True:
+        #Sort the viscosities based no the entry in the first column.
+        phiVsMinViscosity = phiVsMinViscosity[phiVsMinViscosity[:,0].argsort()]
+        phiVsMaxViscosity = phiVsMaxViscosity[phiVsMaxViscosity[:,0].argsort()]
+        
+        phiVsMinViscosity = phiVsMinViscosity[~np.isnan(phiVsMinViscosity).any(axis=1)]
+        phiVsMaxViscosity = phiVsMaxViscosity[~np.isnan(phiVsMaxViscosity).any(axis=1)]
+        
+        (phiM,alphaM,phiJErrorM,alphaErrorM) = fitAlphaPhiJ(phiVsMaxViscosity[:,1], phiVsMaxViscosity[:,0], suspendingViscosity)
+        (phi0,alpha0,phiJError0,alphaError0) = fitAlphaPhiJ(phiVsMinViscosity[:,1], phiVsMinViscosity[:,0], suspendingViscosity)
+        #textstr = '$\phi_0=%.2f$\n$\alpha_0=%.2f$\n$\phi_m=%.2f$\n$\alpha_m=%.2f$' % (phi0, alpha0, phiM,alphaM)
+        
+             
+    
+    
+    pyplot.yscale('log')
+    pyplot.xscale('log')
+    pyplot.xlabel("Shear Stress [Pa]")
+    pyplot.ylabel("Viscosity [Pa s]")
+    pyplot.title("Flow Curves for GdCl Cornstarch Suspensions")
+    pyplot.grid(True)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    if maxMinPtsOn == True:
+        pyplot.text(1700, 6.2, "$\phi_0$ = " + str(round(phi0,4)) + "$\pm$" + str(round(phiJError0,4)),fontsize=14)
+        pyplot.text(1700, 3.8, "$\phi_M$ = " + str(round(phiM,4))+ "$\pm$" + str(round(phiJErrorM,4)),fontsize=14)
+        pyplot.text(1700, .0035, r"$\alpha_M$ = " + str(round(alphaM,4))+ "$\pm$" + str(round(alphaErrorM,4)),fontsize=14)
+        pyplot.text(1700, .0065, r"$\alpha_0$ = " + str(round(alpha0,4))+ "$\pm$" + str(round(alphaError0,4)),fontsize=14)
+#        ax.text(2, 1, "$\phi_0$ = " + str(round(phi0,2)))
+#        ax.text(1600, 4, "$alpha_0$ = " + str(round(alpha0,2)))
+        #ax.text(1600, 0, "$\phi_M$ = " + str(round(phiM,2)))
+        #ax.text(1600, 0, "$alpha_M$ = " + str(round(alphaM,2)))
+    # Put a legend to the right of the current axis
+    
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    
+        
+    if maxMinPtsOn == True:
+        return (phiVsMinViscosity,phiVsMaxViscosity,phiM,alphaM,phi0,alpha0)
+    
+        
+
+def fitAlphaPhiJ(viscosityValues, packingFractionValues, suspendingViscosity):
+    
+    def divergingViscosityFunc(x,phiJ, alpha):
+        
+        return suspendingViscosity * ( 1 - (x/phiJ) )**( -alpha )
+    
+    
+    (popt, pcov) = curve_fit(divergingViscosityFunc, packingFractionValues, viscosityValues,bounds=(0, [1, 100]))
+    
+    alpha = popt[1]
+    phiJ = popt[0]
+    errors = np.sqrt(np.diag(pcov))
+    alphaError = errors[1]
+    phiJError = errors[0]
+    
+    
+    return (phiJ,alpha,phiJError,alphaError)
+
+
+def viscosityDivergencePlot(lowViscosityValues,lowViscosityValuesGdCl, packingFractionValuesGdCl,packingFractionValuesCornstarch, suspendingViscosityCornstarch,suspendingViscosityGdCl):
+    
+    
+    def divergingViscosityFuncGdCl(x,phiJ, alpha):
+        return suspendingViscosityGdCl * ( 1 - (x/phiJ) )**( -alpha )
+    
+    def divergingViscosityFuncNormal(x,phiJ, alpha):
+        return suspendingViscosityCornstarch * ( 1 - (x/phiJ) )**( -alpha )
+    
+    (phiJGdCl,alphaGdCl,phiJErrorG,alphaErrorG) = fitAlphaPhiJ(lowViscosityValuesGdCl, packingFractionValuesGdCl, suspendingViscosityGdCl)
+    (phiJ0,alpha0,phiJError0,alphaError0) = fitAlphaPhiJ(lowViscosityValues, packingFractionValuesCornstarch, suspendingViscosityCornstarch)
+    
+#    phiJLineYValues = np.linspace(0,max(highViscosityValues),50)
+#    phiJMLineXValues = phiJGdCl*np.ones((50))
+#    phiJ0LineXValues = phiJ0*np.ones((50))
+    
+    
+    packingFractionValuesGdClXValues = np.linspace(0,.37,100)
+    packingFractionValuesCornstarchXValues = np.linspace(0,.37,100)
+    
+    
+    pyplot.plot(packingFractionValuesGdCl,lowViscosityValuesGdCl,'o',color='C0')
+    pyplot.plot(packingFractionValuesCornstarch,lowViscosityValues,'o',color='C1')
+    pyplot.plot(packingFractionValuesGdClXValues,divergingViscosityFuncGdCl(packingFractionValuesGdClXValues,phiJGdCl,alphaGdCl),color='C0')
+    pyplot.plot(packingFractionValuesCornstarchXValues,divergingViscosityFuncNormal(packingFractionValuesCornstarchXValues,phiJ0,alpha0),color='C1')
+#    pyplot.plot(phiJMLineXValues,phiJLineYValues,'.')
+#    pyplot.plot(phiJ0LineXValues,phiJLineYValues,'.')
+    pyplot.legend(("GdCl Cornstarch Suspension","Normal Cornstarch Suspension","GdCl Fit","Normal Cornstarch Fit"))
+    pyplot.title("Frictional Newtonian Viscosities Vs Packing Fraction")
+    pyplot.xlabel("Packing Fraction $\phi$")
+    pyplot.ylabel("Viscosity [Pa s]")
+    
+    
+    
+def tauStarFit(newtonianViscosityValues,packingFractionValues,stressValues,phiM,phi0,alpha,suspendingViscosity):
+    
+    #X is a tuple that is X = (packingFractionValues,stressValues)
+    def viscosityFunc(X,tauStar):
+        packingFractionValues,stressValues = X
+        
+        return suspendingViscosity * (1- packingFractionValues/(phiM*(1-stressValues/tauStar)+phi0*(1-stressValues/tauStar)) )**alpha
+    
+    (popt, pcov) = curve_fit(viscosityFunc, (packingFractionValues,stressValues), newtonianViscosityValues,bounds=(0, [1, 100]))
+    
+
+def logarithmicPlotter(viscosityValues0,viscosityValues1,packingFractionValues0,packingFractionValues1,suspendingViscosity0,alpha0,phiJ0,suspendingViscosity1,alpha1,phiJ1,highOrLow):
+    
+    #logEta0 = np.log10(suspendingViscosity0)*np.ones(len(viscosityValues0))
+    logViscosityValues0 = np.log10(viscosityValues0)
+    logPackingFraction0 = np.log10(1-packingFractionValues0/phiJ0)
+    
+    #logEta1 = np.log10(suspendingViscosity1)*np.ones(len(viscosityValues1))
+    logViscosityValues1 = np.log10(viscosityValues1)
+    logPackingFraction1 = np.log10(1-packingFractionValues1/phiJ1)
+    
+    
+    pyplot.plot(logPackingFraction0,logViscosityValues0,'o')
+    
+    pyplot.plot(logPackingFraction1,logViscosityValues1,'o')
+    #pyplot.title()
+    if highOrLow == "high":
+        pyplot.xlabel(r"$\log(1-\frac{\phi}{\phi_M})$")
+        pyplot.title("Frictional Newtonian Regimes")
+        
+    if highOrLow == "low":
+        pyplot.xlabel(r"$\log(1-\frac{\phi}{\phi_0})$")
+        pyplot.title("Frictionless Newtonian Regimes")
+    
+        
+    pyplot.legend(("Pure Cornstarch Suspension","GdCl Cornstarch Suspension"))
+    pyplot.ylabel(r"$\log(\eta)$")
+    
+    font = {'family' : 'normal',
+
+        'size'   : 10}
+
+    pyplot.rc('font', **font)
+    #pyplot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    
+    
+def logarithmicPlotterAllLines(phiVsMinViscosityG,phiVsMinViscosityC,phiVsMaxViscosityG,phiVsMaxViscosityC,phiMG,alphaMG,phi0G,alpha0G,phiMC,alphaMC,phi0C,alpha0C,suspendViscosityC,suspendViscosityG):
+    
+    
+#    logEta0C_0 = np.log10(suspendViscosityC)*np.ones(len(phiVsMinViscosityC[:,1]))
+#    logEta0G_0 = np.log10(suspendViscosityG)*np.ones(len(phiVsMinViscosityG[:,1]))
+#    
+#    
+#    logEta0C_M = np.log10(suspendViscosityC)*np.ones(len(phiVsMaxViscosityC[:,1]))
+#    logEta0G_M = np.log10(suspendViscosityG)*np.ones(len(phiVsMaxViscosityG[:,1]))    
+#    
+    
+    
+    logViscosityValuesC_0= np.log10(phiVsMinViscosityC[:,1])
+    logPackingFractionC_0 = np.log10(1-phiVsMinViscosityC[:,0]/phi0C)
+    logViscosityValuesG_0= np.log10(phiVsMinViscosityG[:,1])
+    logPackingFractionG_0 = np.log10(1-phiVsMinViscosityG[:,0]/phi0G)
+    
+    logViscosityValuesC_M= np.log10(phiVsMaxViscosityC[:,1])
+    logPackingFractionC_M = np.log10(1-phiVsMaxViscosityC[:,0]/phiMC)
+    logViscosityValuesG_M= np.log10(phiVsMaxViscosityG[:,1])
+    logPackingFractionG_M = np.log10(1-phiVsMaxViscosityG[:,0]/phiMG)
+    
+    
+    
+    pyplot.plot(logPackingFractionC_0,logViscosityValuesC_0,'o',label="Frictionless Cornstarch")
+    pyplot.plot(logPackingFractionG_0,logViscosityValuesG_0,'o',label="Frictionless Cornstarch/GdCl")
+    pyplot.plot(logPackingFractionC_M,logViscosityValuesC_M,'o',label="Frictional Cornstarch")
+    pyplot.plot(logPackingFractionG_M,logViscosityValuesG_M,'o',label="Frictional Cornstarch/GdCl")
+    
+    
+    pyplot.legend()
+    pyplot.xlabel(r"$\log(1-\frac{\phi}{\phi_J})$")
+    pyplot.ylabel(r"$\log(\eta)$")
